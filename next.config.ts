70e91a48cecaeed9aa9f3940a65e663ad49677c5
Praneeth Badugu <praneethbadugu1530@gmail.com>
2026-05-11 17:08:34 +0530

    Fix: Add Razorpay to CSP script-src directive

--- next.config.ts
+++ next.config.ts
@@ -39,7 +39,7 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self'",
-              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://sdk.cashfree.com https://www.googletagmanager.com https://www.google-analytics.com https://accounts.google.com https://apis.google.com",
+              "script-src 'self' 'unsafe-inline' 'unsafe-eval' https://sdk.cashfree.com https://checkout.razorpay.com https://www.googletagmanager.com https://www.google-analytics.com https://accounts.google.com https://apis.google.com",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://accounts.google.com",
               "font-src 'self' https://fonts.gstatic.com",
               "img-src 'self' data: blob: https://images.unsplash.com https://res.cloudinary.com https://www.google-analytics.com https://lh3.googleusercontent.com",
