1a7b50c1ac0fa303a99b65ebcfdbd7a24ee01f2e
Praneeth Badugu <praneethbadugu1530@gmail.com>
2026-05-13 20:47:47 +0530

    Fix: Simplify Prisma initialization for production stability

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
 
 declare global {
   // eslint-disable-next-line no-var
@@ -7,8 +6,7 @@ declare global {
 }
 
 function createPrisma() {
-  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-  return new PrismaClient({ adapter });
+  return new PrismaClient();
 }
 
 export const prisma = global.prisma ?? createPrisma();
